2aeed73109df8cd81edc9a4a4c2fad846ba22d67
Rain Holloway <rainh2001@gmail.com>
2022-09-19 12:14:28 +1000

    Working on database testing v2.0

--- src/server/src/utils/prisma.ts
+++ src/server/src/utils/prisma.ts
@@ -11,20 +11,31 @@ export async function resetClient() {
 
 export async function createTestClient() {
   if (process.env.NODE_ENV === 'test') {
+    await resetClient();
+    await dropTestDatabase();
     await prisma.$executeRaw`CREATE DATABASE RadioFMTest`;
+
     const databaseArr = process.env.DATABASE_URL?.split('/');
     databaseArr?.pop();
     databaseArr?.push(getDatabaseName());
     process.env.DATABASE_URL = databaseArr?.join('/');
     console.log(process.env.DATABASE_URL);
+
+    await prisma.$disconnect();
     await resetClient();
   }
 }
 
 export async function deleteTestClient() {
   if (process.env.NODE_ENV === 'test') {
-    await prisma.$executeRaw`DROP DATABASE IF EXISTS RadioFMTest`;
+    await dropTestDatabase();
     await prisma.$disconnect();
+  }
+}
+
+async function dropTestDatabase() {
+  if (process.env.NODE_ENV === 'test') {
+    await prisma.$executeRaw`DROP DATABASE IF EXISTS RadioFMTest`;
   }
 }
 
