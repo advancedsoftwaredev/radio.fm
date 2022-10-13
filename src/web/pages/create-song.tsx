a82eacfdd6b1489e220f30d7d9f3914fc66aaa7e
Rain Holloway <rainh2001@gmail.com>
2022-10-13 16:57:28 +1100

    Changes to UI

--- src/web/pages/create-song.tsx
+++ src/web/pages/create-song.tsx
@@ -2,6 +2,7 @@ import { Box, Button, TextField, Typography } from '@mui/material';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
+import Header from '../components/Header';
 import { useUserData } from '../components/hooks/userContext';
 import { api } from '../util/api';
 
@@ -70,102 +71,111 @@ const CreateSong = () => {
   };
 
   return (
-    <Box
-      sx={{ height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}
-    >
-      <form onSubmit={(e) => e.preventDefault()}>
-        <h1>Create Song</h1>
-        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          <Box sx={{ marginBottom: '1rem', display: 'flex', flexDirection: 'column' }}>
-            <TextField
-              type="text"
-              sx={inputStyle}
-              variant="filled"
-              label="Artist"
-              placeholder="Artist"
-              value={artist}
-              onChange={(e) => setArtist(e.target.value)}
-              required={true}
-            />
-          </Box>
-          <Box sx={{ marginBottom: '1rem', display: 'flex', flexDirection: 'column' }}>
-            <TextField
-              type="text"
-              sx={inputStyle}
-              variant="filled"
-              label="Title"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              required={true}
-            />
-          </Box>
-
-          <Box sx={{ marginBottom: '1rem', display: 'flex', flexDirection: 'column' }}>
-            <TextField
-              type="text"
-              sx={inputStyle}
-              variant="filled"
-              label="Description"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              required={true}
-            />
-          </Box>
-
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <Button variant="contained" component="label" sx={{ alignSelf: 'start' }}>
-              Upload Album Cover
-              <input
-                id="album-cover-upload"
-                hidden={true}
-                accept="image/*"
-                type="file"
-                onChange={(e) => {
-                  if (e.target.files) {
-                    setImageFile(e.target.files[0]);
-                  }
-                }}
+    <>
+      <Header />
+      <Box
+        sx={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <form onSubmit={(e) => e.preventDefault()}>
+          <h1>Create Song</h1>
+          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Box sx={{ marginBottom: '1rem', display: 'flex', flexDirection: 'column' }}>
+              <TextField
+                type="text"
+                sx={inputStyle}
+                variant="filled"
+                label="Artist"
+                placeholder="Artist"
+                value={artist}
+                onChange={(e) => setArtist(e.target.value)}
+                required={true}
               />
-            </Button>
-            {imageFile?.name && <Typography sx={{ marginLeft: '1rem' }}>{imageFile.name}</Typography>}
-          </Box>
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <Button
-              variant="contained"
-              component="label"
-              sx={{ marginTop: '1rem', marginBottom: '1rem', alignSelf: 'start' }}
-            >
-              Upload Song MP3
-              <input
-                id="album-cover-upload"
-                hidden={true}
-                accept="audio/*"
-                type="file"
-                onChange={(e) => {
-                  if (e.target.files) {
-                    setAudioFile(e.target.files[0]);
-                  }
-                }}
-              />
-            </Button>
-            {audioFile?.name && <Typography sx={{ marginLeft: '1rem' }}>{audioFile.name}</Typography>}
-          </Box>
-          {error && (
-            <Box sx={{ marginBottom: '1rem' }}>
-              <Typography color="error">{error}</Typography>
             </Box>
-          )}
-          <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '30rem' }}>
-            <Button variant="outlined" onClick={() => router.push('/song-management')}>
-              Cancel
-            </Button>
-            <Button variant="contained" type="submit" onClick={() => createSong()}>
-              {loading ? 'Loading...' : 'Create Song'}
-            </Button>
+            <Box sx={{ marginBottom: '1rem', display: 'flex', flexDirection: 'column' }}>
+              <TextField
+                type="text"
+                sx={inputStyle}
+                variant="filled"
+                label="Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                required={true}
+              />
+            </Box>
+
+            <Box sx={{ marginBottom: '1rem', display: 'flex', flexDirection: 'column' }}>
+              <TextField
+                type="text"
+                sx={inputStyle}
+                variant="filled"
+                label="Description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                required={true}
+              />
+            </Box>
+
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <Button variant="contained" component="label" sx={{ alignSelf: 'start' }}>
+                Upload Album Cover
+                <input
+                  id="album-cover-upload"
+                  hidden={true}
+                  accept="image/*"
+                  type="file"
+                  onChange={(e) => {
+                    if (e.target.files) {
+                      setImageFile(e.target.files[0]);
+                    }
+                  }}
+                />
+              </Button>
+              {imageFile?.name && <Typography sx={{ marginLeft: '1rem' }}>{imageFile.name}</Typography>}
+            </Box>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <Button
+                variant="contained"
+                component="label"
+                sx={{ marginTop: '1rem', marginBottom: '1rem', alignSelf: 'start' }}
+              >
+                Upload Song MP3
+                <input
+                  id="album-cover-upload"
+                  hidden={true}
+                  accept="audio/*"
+                  type="file"
+                  onChange={(e) => {
+                    if (e.target.files) {
+                      setAudioFile(e.target.files[0]);
+                    }
+                  }}
+                />
+              </Button>
+              {audioFile?.name && <Typography sx={{ marginLeft: '1rem' }}>{audioFile.name}</Typography>}
+            </Box>
+            {error && (
+              <Box sx={{ marginBottom: '1rem' }}>
+                <Typography color="error">{error}</Typography>
+              </Box>
+            )}
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '30rem' }}>
+              <Button variant="outlined" onClick={() => router.push('/song-management')}>
+                Cancel
+              </Button>
+              <Button variant="contained" type="submit" onClick={() => createSong()}>
+                {loading ? 'Loading...' : 'Create Song'}
+              </Button>
+            </Box>
           </Box>
-        </Box>
-      </form>
-    </Box>
+        </form>
+      </Box>
+    </>
   );
 };
 
