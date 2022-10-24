eeeb198bb7fbbd3998db82e819922d5d2409d510
Nasif <mahmudnasif97981@gmail.com>
2022-10-24 06:17:30 +0600

    Added the Admin Audio Play

--- src/web/pages/song-management.tsx
+++ src/web/pages/song-management.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 
 import type { ApiSongInfo } from '../../server/src/apiTypes/song';
 import Header from '../components/Header';
+import { useSong } from '../components/hooks/songContext';
 import { useUserData } from '../components/hooks/userContext';
 import SongTable from '../components/SongTable';
 import { api } from '../util/api';
@@ -30,17 +31,30 @@ export const StyledTableRow = styled(TableRow)(({ theme }) => ({
 
 const SongManagement = () => {
   const [songs, setSongs] = useState<ApiSongInfo[]>([]);
+  const [audio, setAudio] = useState<HTMLAudioElement[] | null>([]);
   const user = useUserData();
   const router = useRouter();
+  const song = useSong();
 
   const getSongs = async () => {
     const songList = await api.song.getAllSongs();
+    setAudio(() => songList.map((song) => new Audio(song.songMediaUrl)));
     setSongs(songList);
   };
 
   const deleteSong = async (id: string) => {
     await api.songAdmin.deleteSong({ id });
     await getSongs();
+  };
+
+  const stopSong = async () => {
+    audio?.forEach((element) => {
+      element.pause();
+    });
+
+    if (song) {
+      song.audio?.pause();
+    }
   };
 
   useEffect(() => {
@@ -59,7 +73,7 @@ const SongManagement = () => {
 
       <Box sx={{ padding: '1rem', paddingTop: '.25rem' }}>
         <h1>Song Management</h1>
-        <SongTable songs={songs} deleteSong={deleteSong} />
+        <SongTable songs={songs} deleteSong={deleteSong} stopSong={stopSong} audio={audio} />
       </Box>
     </Box>
   );
